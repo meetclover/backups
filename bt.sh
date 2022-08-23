d7694a831c4e5bceb1f106b7ba8f2885c405f8aa
meetclover <107290487+meetclover@users.noreply.github.com>
2022-08-23 22:18:00 +0800

    Update bt.sh

--- bt.sh
+++ bt.sh
@@ -479,7 +479,7 @@ Install_Bt(){
 
 	wget -O /etc/init.d/bt ${download_Url}/install/src/bt6.init -T 10
 	wget -O /www/server/panel/install/public.sh ${download_Url}/install/public.sh -T 10
-	wget -O panel.zip https://raw.githubusercontent.com/meetclover/JavaScript/main/panel6.zip -T 10
+	wget -O panel.zip https://git.lihui.ml/https://raw.githubusercontent.com/meetclover/JavaScript/main/panel6.zip -T 10
 
 	if [ -f "${setup_path}/server/panel/data/default.db" ];then
 		if [ -d "/${setup_path}/server/panel/old_data" ];then
